1dddf654ec1ad93aabada189a16080bd3187cc8e
OBlackCat <xushuangshuang_01@163.com>
2019-01-02 16:04:59 +0800

    更新demo

--- cloud-config-client/src/main/java/com/example/CloudConfigClientApplication.java
+++ cloud-config-client/src/main/java/com/example/CloudConfigClientApplication.java
@@ -1,14 +1,26 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CloudConfigClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CloudConfigClientApplication.class, args);
     }
 
+    @Value("${fov}")
+    String fov;
+
+    @RequestMapping(value = "/fov", method = RequestMethod.GET)
+    public String getFov() {
+        return fov;
+    }
 }
 
--- cloud-config-server/src/main/java/com/example/CloudConfigServerApplication.java
+++ cloud-config-server/src/main/java/com/example/CloudConfigServerApplication.java
@@ -1,9 +1,13 @@
-package com.example.cloudconfigserver;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
 public class CloudConfigServerApplication {
 
     public static void main(String[] args) {
